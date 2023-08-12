d80c0e4a48306811e349f382313f2f568f02b05d
giuliabaracal <140750477+giuliabaracal@users.noreply.github.com>
2023-08-12 12:52:30 -0300

    feat: creating itens object

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,9 +1,52 @@
-    class CaixaDaLanchonete {
+class CaixaDaLanchonete {
+  constructor() {
+    const itens = [
+      {
+        codigo: "cafe",
+        descricao: "Café",
+        valor: 3,
+      },
+      {
+        codigo: "chantily",
+        descricao: "Chantily (extra do café)",
+        valor: 1.5,
+      },
+      {
+        codigo: "suco",
+        descricao: "Suco Natural",
+        valor: 6.2,
+      },
+      {
+        codigo: "sanduiche",
+        descricao: "Sanduíche",
+        valor: 6.5,
+      },
+      {
+        codigo: "queijo",
+        descricao: "Queijo (extra do Sanduíche)",
+        valor: 2,
+      },
+      {
+        codigo: "salgado",
+        descricao: "Salgado",
+        valor: 7.25,
+      },
+      {
+        codigo: "combo1",
+        descricao: "1 Suco e 1 Sanduíche",
+        valor: 9.5,
+      },
+      {
+        codigo: "combo2",
+        descricao: "1 Café e 1 Sanduíche",
+        valor: 7.5,
+      },
+    ];
+  }
 
-    calcularValorDaCompra(metodoDePagamento, itens) {
-        return "";
-    }
-
+  calcularValorDaCompra(metodoDePagamento, itens) {
+    return "";
+  }
 }
 
 export { CaixaDaLanchonete };
